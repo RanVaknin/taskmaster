8411f11efe8ff58530f5a6fdc315b6518e399a9e
RAN VAKNIN <ranvaknin0@gmail.com>
2020-02-11 11:30:40 -0800

    first commit

---
+++ app/src/main/java/com/example/taskmaster/AddTask.java
@@ -0,0 +1,27 @@
+package com.example.taskmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class AddTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addtask);
+
+        Button b = findViewById(R.id.button);
+
+        b.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Toast.makeText(AddTask.this, "submitted!", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
+}
