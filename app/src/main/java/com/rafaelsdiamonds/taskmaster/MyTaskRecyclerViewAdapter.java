e2592d25e68271516310ae3e763b0b6603bf8f5b
RAN VAKNIN <ranvaknin0@gmail.com>
2020-02-13 10:26:58 -0800

    broken squiggles

---
+++ app/src/main/java/com/rafaelsdiamonds/taskmaster/MyTaskRecyclerViewAdapter.java
@@ -0,0 +1,82 @@
+package com.rafaelsdiamonds.taskmaster;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.taskmaster.R;
+import com.rafaelsdiamonds.taskmaster.TaskFragment.OnListFragmentInteractionListener;
+import com.rafaelsdiamonds.taskmaster.dummy.DummyContent.DummyItem;
+
+import java.util.List;
+
+/**
+ * {@link RecyclerView.Adapter} that can display a {@link DummyItem} and makes a call to the
+ * specified {@link OnListFragmentInteractionListener}.
+ * TODO: Replace the implementation with code for your data type.
+ */
+public class MyTaskRecyclerViewAdapter extends RecyclerView.Adapter<MyTaskRecyclerViewAdapter.ViewHolder> {
+
+    private final String TAG = "rvrv";
+    private final List<Task> mValues;
+    private final OnListFragmentInteractionListener mListener;
+
+    public MyTaskRecyclerViewAdapter(List<Task> items, OnListFragmentInteractionListener listener) {
+        mValues = items;
+        mListener = listener;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_task, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.mTitleView.setText(mValues.get(position).getTitle());
+        holder.mBodyView.setText(mValues.get(position).getBody());
+        holder.mStateView.setText(mValues.get(position).getState());
+
+
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "click!");
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final TextView mTitleView;
+        public final TextView mBodyView;
+        public final TextView mStateView;
+        public Task mItem;
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+            mTitleView = (TextView) view.findViewById(R.id.title);
+            mBodyView = (TextView) view.findViewById(R.id.body);
+            mStateView = (TextView) view.findViewById(R.id.state);
+
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mBodyView.getText() + "'";
+        }
+    }
+}
---
+++ app/src/main/java/com/rafaelsdiamonds/taskmaster/Task.java
@@ -0,0 +1,44 @@
+package com.rafaelsdiamonds.taskmaster;
+
+
+public class Task {
+    private String title;
+    private String body;
+    private String state;
+
+
+    public Task(String title, String body) {
+        this.title = title;
+        this.body = body;
+//        this.state = state;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public void setState(String state) {
+        if(state.toLowerCase().equals("new") || state.toLowerCase().equals("in progress")
+        || state.toLowerCase().equals("complete") || state.toLowerCase().equals("assigned")){
+        this.state = state;
+
+        }
+    }
+}
