d6e77d83b49491d1ad1545e1b9b1e24e919de6b7
RAN VAKNIN <ranvaknin0@gmail.com>
2020-02-25 13:57:00 -0800

    not done with auth yet

--- app/src/main/java/com/rafaelsdiamonds/taskmaster/MainActivity.java
+++ app/src/main/java/com/rafaelsdiamonds/taskmaster/MainActivity.java
@@ -8,14 +8,21 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.amazonaws.amplify.generated.graphql.ListTasksQuery;
 import com.amazonaws.amplify.generated.graphql.ListTeamsQuery;
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobile.client.Callback;
+import com.amazonaws.mobile.client.SignOutOptions;
+import com.amazonaws.mobile.client.UserState;
+import com.amazonaws.mobile.client.UserStateDetails;
 import com.amazonaws.mobile.config.AWSConfiguration;
 import com.amazonaws.mobileconnectors.appsync.AWSAppSyncClient;
+import com.amazonaws.services.cognitoidentityprovider.model.GetUserAttributeVerificationCodeRequest;
 import com.example.taskmaster.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
     Team teamSprite;
     Team teamPepsi;
     Team teamCoke;
+    String username = "";
 
     String[] taskName;
     String[] taskState;
@@ -43,7 +51,45 @@ public class MainActivity extends AppCompatActivity {
                 .build();
 
 
+        // auth initialization
+        AWSMobileClient.getInstance().initialize(getApplicationContext(), new Callback<UserStateDetails>() {
+
+                    @Override
+                    public void onResult(UserStateDetails userStateDetails) {
+                        Log.i("rvrv", "onResult: " + userStateDetails.getUserState());
+                        if (userStateDetails.getUserState().equals(UserState.SIGNED_OUT)) {
+                            loginUi();
+                        }
+                    }
+
+                    @Override
+                    public void onError(Exception e) {
+                        Log.e("rvrv", "Initialization error.", e);
+                    }
+                }
+        );
+
+
+
         Button addTaskButton = findViewById(R.id.add_task);
+        addTaskButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AWSMobileClient.getInstance().signOut(SignOutOptions.builder().signOutGlobally(true).build(), new Callback<Void>() {
+                    @Override
+                    public void onResult(final Void result) {
+                        Log.d("rvrv", "signed-out");
+                    }
+
+                    @Override
+                    public void onError(Exception e) {
+                        Log.e("rvrv", "sign-out error", e);
+                    }
+                });
+                AWSMobileClient.getInstance().signOut();
+                loginUi();
+            }
+        });
 
         addTaskButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -75,6 +121,27 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void loginUi(){
+        AWSMobileClient.getInstance().showSignIn(MainActivity.this, new Callback<UserStateDetails>() {
+            @Override
+            public void onResult(UserStateDetails result) {
+                Log.d("rvrv", "onResult: " + result.getUserState());
+                try {
+                    username = AWSMobileClient.getInstance().getUserAttributes().get("given_name");
+                }
+                catch(Exception e) {
+                    Log.e("rvrv", "onError: ", e);
+                }
+
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Log.e("rvrv", "onError: ", e);
+            }
+        });
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -86,6 +153,8 @@ public class MainActivity extends AppCompatActivity {
             myTasks.setText(usernameDisplay + "'s Tasks");
 
         }
+
+        setTitle(username + "'s tasks");
 
 
     }
