af01948698eaa290bfbb3bdbe9158c70af28e27b
RAN VAKNIN <ranvaknin0@gmail.com>
2020-03-04 13:42:34 -0800

    finished location services

--- app/src/main/java/com/rafaelsdiamonds/taskmaster/TaskDetail.java
+++ app/src/main/java/com/rafaelsdiamonds/taskmaster/TaskDetail.java
@@ -1,50 +1,14 @@
 package com.rafaelsdiamonds.taskmaster;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.TextView;
-
-import com.amazonaws.amplify.generated.graphql.CreateTaskMutation;
-import com.amazonaws.amplify.generated.graphql.ListTeamsQuery;
 import com.amazonaws.mobile.config.AWSConfiguration;
 import com.amazonaws.mobileconnectors.appsync.AWSAppSyncClient;
-import com.apollographql.apollo.GraphQLCall;
-import com.apollographql.apollo.api.Response;
-import com.apollographql.apollo.exception.ApolloException;
 import com.squareup.picasso.Picasso;
-
-import org.xml.sax.InputSource;
-import org.xml.sax.XMLReader;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.List;
-
-import javax.annotation.Nonnull;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-
-import type.CreateTaskInput;
 
 public class TaskDetail extends AppCompatActivity {
 
@@ -71,6 +35,9 @@ public class TaskDetail extends AppCompatActivity {
         ImageView taskImage = findViewById(R.id.taskImage);
 
         Picasso.get().load(imgForThisTask).into(taskImage);
+
+        TextView locationText = findViewById(R.id.locationText);
+        locationText.setText(getIntent().getStringExtra("TaskLocation"));
 
 //       Log.e("rvrv", "assssss : " + imgForThisTask);
 
